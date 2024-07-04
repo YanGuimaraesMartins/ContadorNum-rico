eeb1a38d09a0d2ff77d58d98566decc62f2413c1
YanGuimaraesMartins <134331115+YanGuimaraesMartins@users.noreply.github.com>
2024-07-04 11:10:13 -0300

    Add files via upload

---
+++ src/ProjetoContador/Contador.java
@@ -0,0 +1,36 @@
+package ProjetoContador;
+import java.util.Scanner;
+
+public class Contador {
+
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		while (true) {
+			System.out.println("Digite o primeiro parâmetro");
+			int parametroUm = terminal.nextInt();
+			System.out.println("Digite o segundo parâmetro");
+			int parametroDois = terminal.nextInt();
+			int resultado = contagem(parametroUm, parametroDois);
+			try {
+				exibir(resultado);
+				break;
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+
+	public static int contagem(int parametroUm, int parametroDois) {
+		return parametroDois - parametroUm;
+	}
+
+	public static void exibir(int contar) throws Exception {
+		if (contar < 0) {
+			throw new Exception("O primeiro número deve ser menor que o segundo");
+		} else
+			for (int i = 1; i <= contar; i++) {
+				System.out.println("Número " + i);
+			}
+	}
+
+}
